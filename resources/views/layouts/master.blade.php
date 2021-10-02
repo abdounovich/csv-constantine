4a45b540e903333321fba42a237950e51de7eccb
bob <abdounovich@gmail.com>
2021-10-02 22:55:11 +0200

    comment

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,8 +2,7 @@
     <head>
         <title> @yield('title')</title>
         <meta charset="UTF-8">
-        <meta  name="viewport" content="width= {screenWidth}">
-
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta http-equiv="refresh" content="250">
